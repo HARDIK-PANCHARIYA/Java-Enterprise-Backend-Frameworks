1c07ffb858472ce18129f25137aebe12c8fadd2f
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-10-30 23:23:15 +0530

    Hibernate

---
+++ HibernateProj/src/main/java/com/core/crudoperations/UpdateOperation.java
@@ -0,0 +1,26 @@
+package com.core.crudoperations;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.core.entityes.User;
+
+public class UpdateOperation {
+	
+	public static void main(String[] args) {
+		
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		cfg.addAnnotatedClass(User.class);
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		User u = new User(106,"XYZ","xyz@gmail.com","xyz@1234");
+		session.update(u);
+		Transaction tx = session.beginTransaction();
+		tx.commit();
+		System.out.println("Update successfully");
+	}
+
+}
