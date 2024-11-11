12db05575a1097a8cf1ca0996329e66c673fe741
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-11-11 23:24:01 +0530

    SpringBoot

---
+++ SpringBootProj_189/src/test/java/com/core/SpringBootProj_189/SpringBootProj189ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.SpringBootProj_189;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootProj189ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
