6a970203610631ecef8fa34a2be5f800248d9294
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-11-20 23:02:14 +0530

    Spring MVC

---
+++ SpringMvcProj/src/test/java/com/core/springmvc/SpringMvcProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
