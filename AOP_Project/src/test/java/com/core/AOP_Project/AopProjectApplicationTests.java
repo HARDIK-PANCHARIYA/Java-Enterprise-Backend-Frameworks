92895c0c1da4e84203d90ac0e25ad4c75fa691f4
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-11-21 23:19:32 +0530

    Aspect-oriented Programming (AOP)

---
+++ AOP_Project/src/test/java/com/core/AOP_Project/AopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.AOP_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
