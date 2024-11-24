a2de5bac7a7c8381f986b6fcec6f26a0ad58fafc
HARDIK-PANCHARIYA <hardikpanchariya78@gmail.com>
2024-11-24 09:47:33 +0530

    Servlet

---
+++ ServletProj_189/src/main/java/com/core/servlet/SecondServlet.java
@@ -0,0 +1,29 @@
+package com.core.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/second")
+public class SecondServlet extends HttpServlet {
+	
+    public SecondServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h2 style='color:green'>Welcome to second servlet</h2>");
+		
+	}
+
+}
